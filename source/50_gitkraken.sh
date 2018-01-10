47cbde83fe8517ecfad686c7c70d4125cdcc815b
Austin Rivas <austinrivas@gmail.com>
2018-01-10 16:55:51 -0500

    removed uneccessary test function in gk launcher

--- source/50_gitkraken.sh
+++ source/50_gitkraken.sh
@@ -1,5 +1,7 @@
 GITKRAKEN_LISTEN_LIMIT=524288
 
+# Increasing the amount of inotify watchers
+# https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
 function raise_listen_limit() {
     sudo sysctl fs.inotify.max_user_watches=$LISTEN_LIMIT
     sudo sysctl -p
@@ -22,8 +24,17 @@ function test_gitkraken_listen_limit() {
     fi
 }
 
+# Raise the inotify listen limit and launch gitkraken
 function wake_the_kraken() {
-    test_gitkraken_listen_limit gitkraken $1
+    CURRENT_LIMIT=$(get_current_listen_limit)
+    echo "GK Listen Limit : $GITKRAKEN_LISTEN_LIMIT";
+    echo "Current Limit : $CURRENT_LIMIT";
+    if [[ $CURRENT_LIMIT -lt $GITKRAKEN_LISTEN_LIMIT ]]; then
+        raise_listen_limit
+        echo "Waking the Kraken!";
+    fi
+
+    gitkraken $1
 }
 
 alias ~~='wake_the_kraken';
