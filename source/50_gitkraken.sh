86410da08e57affeb22f602812f806734aa5d6f3
Austin Rivas <austinrivas@gmail.com>
2018-01-10 19:08:38 -0500

    removed unused test function and made raise limit reusable

--- source/50_gitkraken.sh
+++ source/50_gitkraken.sh
@@ -3,25 +3,12 @@ GITKRAKEN_LISTEN_LIMIT=524288
 # Increasing the amount of inotify watchers
 # https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
 function raise_listen_limit() {
-    sudo sysctl fs.inotify.max_user_watches=$LISTEN_LIMIT
+    sudo sysctl fs.inotify.max_user_watches=$1
     sudo sysctl -p
 }
 
 function get_current_listen_limit() {
     sudo cat /proc/sys/fs/inotify/max_user_watches
-}
-
-function test_gitkraken_listen_limit() {
-    CURRENT_LIMIT=$(get_current_listen_limit)
-    echo "GK Listen Limit : $GITKRAKEN_LISTEN_LIMIT";
-    echo "Current Limit : $CURRENT_LIMIT";
-    if [[ $CURRENT_LIMIT -lt $GITKRAKEN_LISTEN_LIMIT ]]; then
-        echo "Raise the limit to wake the kraken.";
-    else
-        raise_listen_limit
-        echo "Waking the kraken."
-        $1 $2
-    fi
 }
 
 # Raise the inotify listen limit and launch gitkraken
@@ -30,7 +17,7 @@ function wake_the_kraken() {
     echo "GK Listen Limit : $GITKRAKEN_LISTEN_LIMIT";
     echo "Current Limit : $CURRENT_LIMIT";
     if [[ $CURRENT_LIMIT -lt $GITKRAKEN_LISTEN_LIMIT ]]; then
-        raise_listen_limit
+        raise_listen_limit $GITKRAKEN_LISTEN_LIMIT
         echo "Waking the Kraken!";
     fi
 
