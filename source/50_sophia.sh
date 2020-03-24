8510937c491174fd060f8c0aa6b0bb1d19142bf4
Austin Rivas <austinrivas@gmail.com>
2020-03-25 02:48:04 +0700

    added sophia aliases

---
+++ source/50_sophia.sh
@@ -0,0 +1,5 @@
+local SOPHIA_HOME=~/Workspace/Sophia
+alias sophia="cd $SOPHIA_HOME"
+alias sophia_docs="cd $SOPHIA_HOME/documentation"
+alias open_sophia_docs="sophia_docs && gpu"
+alias tdpk="cd $SOPHIA_HOME/deployment-tdpk"
