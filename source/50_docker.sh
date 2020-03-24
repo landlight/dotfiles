1934c2d66c8af6342e0967b6128cc397b8613983
Austin Rivas <austinrivas@gmail.com>
2020-03-25 03:10:29 +0700

    added docker convenience functions

--- source/50_docker.sh
+++ source/50_docker.sh
@@ -1,9 +1,21 @@
 # https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
 
-function docker_cleanup () {
-    docker system prune
-}
+## Shorthand for docker commands
+alias d='docker'
+alias dm='docker-compose'
+alias dr='docker run -ti $1 $2'
 
-function test_docker() {
-    docker run --rm hello-world
-}
+## Easy logging
+## dlog service-gateway-api
+alias dlog='docker logs --tail 200 $1 -f'
+
+## Stop any currently running docker container and run "yarn dev" in the current directory
+## cd service-gateway-api && dev
+alias dev='docker stop $(basename $(pwd)) && yarn dev'
+
+## Commands to help us cleanup our docker stack
+alias drm='docker rm $(docker ps -a -q)'
+alias dstop='docker stop $(docker ps -a -q)'
+alias dprune='docker system prune -a; docker images purge'
+alias dclean='sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log"'
+alias dtest='docker run --rm hello-world'
