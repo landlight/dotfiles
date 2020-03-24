859d97ceed2259f97d4a2b9f281a1e8b89e46693
Austin Rivas <austinrivas@gmail.com>
2020-03-25 03:05:30 +0700

    moved sophia ssh commands

--- source/50_sophia.sh
+++ source/50_sophia.sh
@@ -3,3 +3,23 @@ alias sophia="cd $SOPHIA_HOME"
 alias sophia_docs="cd $SOPHIA_HOME/documentation"
 alias open_sophia_docs="sophia_docs && gpu"
 alias tdpk="cd $SOPHIA_HOME/deployment-tdpk"
+
+alias tunnel="tunnels"
+function tunnels() {
+    ## Tunnel through the jump server
+    ssh -J jump "$1" -Nf -L "$2":localhost:"$3"
+    ps -ef | grep "$3"
+}
+
+## Create a ssh connection through a jump tunnel
+function sshj_generator() {
+    ssh -J $1 ${@:2}
+}
+
+## Sophia specific jump targets
+function scpj() {
+    scp -o 'ProxyJump jump' $@
+}
+function sshj() {
+    sshj_generator jump $@
+}
