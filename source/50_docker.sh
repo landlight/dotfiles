182e0104886016f68043077e1a0d348103df49b6
Austin Rivas <austinrivas@gmail.com>
2020-03-25 02:19:54 +0700

    added docker_cleanup

--- source/50_docker.sh
+++ source/50_docker.sh
@@ -1,23 +1,9 @@
-# Official Docker Installation Instructions
-# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce-1
-function install_docker() {
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-       $(lsb_release -cs) \
-       stable"
-    sudo apt update
-    sudo apt install docker-ce
-}
+# https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
 
-# Run docker as non-root user
-# https://docs.docker.com/engine/installation/linux/linux-postinstall/#manage-docker-as-a-non-root-user
-function docker_unroot() {
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
+function docker_cleanup () {
+    docker system prune
 }
 
 function test_docker() {
-    docker run hello-world
+    docker run --rm hello-world
 }
