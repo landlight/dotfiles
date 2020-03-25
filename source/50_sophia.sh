6d7b91f3663f7dc29b5680dd55f3a47c521b967f
Austin Rivas <austinrivas@gmail.com>
2020-03-25 15:36:13 +0700

    added tdpk

--- source/50_sophia.sh
+++ source/50_sophia.sh
@@ -3,6 +3,7 @@ alias sophia="cd $SOPHIA_HOME"
 alias sophia_docs="cd $SOPHIA_HOME/documentation"
 alias open_sophia_docs="sophia_docs && gpu"
 alias tdpk="cd $SOPHIA_HOME/deployment-tdpk"
+alias tdpk_up="tdpk && ./deploy.sh"
 
 alias tunnel="tunnels"
 function tunnels() {
