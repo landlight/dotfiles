42b34d04a5498c0aa78a1e9aff706a82827bd499
Austin Rivas <austinrivas@gmail.com>
2020-04-05 23:11:32 +0700

    added find_and_kill_process_on_port

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -35,8 +35,8 @@ function create_file_from_clipboard() {
 }
 
 # find process running on a given port
-function find_process_running_on_port() {
-  lsof -i :$1
+function find_and_kill_process_on_port() {
+  lsof -i tcp:$1 | awk 'NR!=1 {print $2}' | xargs kill
 }
 
 # gets the current unix epoch
